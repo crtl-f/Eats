670212a570c41da6328009f1da835bc550b5786f
crtl-f <derik.latimer@csuglobal.edu>
2024-06-18 13:29:10 -0600

    First Commit

---
+++ ABQ/src/Eats/ABQFood.java
@@ -0,0 +1,18 @@
+package Eats;
+
+public class ABQFood {
+	public static void main(String[] args) {
+		String restaurantName = "Frontier";
+		String businessAddress = "2400 Central Ave.";
+		String city = "Albuquerque";
+		String state = "New Mexico";
+		String zipCode = "87106";
+		
+		System.out.println("Restaurant name: " + restaurantName);
+		System.out.println("Business address: " + businessAddress);
+		System.out.println("City, State, Zip: " + city +  ","  + state +  ","  + zipCode);
+		
+
+	}
+
+}
